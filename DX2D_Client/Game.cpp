f116ec106352258ebbb8d104830e19bbf14f1cbf
JunmoYang <Juunnmmoo@gmail.com>
2024-10-04 16:40:22 +0900

    Add Game Class

--- DX2D_Client/Game.cpp
+++ DX2D_Client/Game.cpp
@@ -1,2 +1,29 @@
 #include "pch.h"
 #include "Game.h"
+
+Game::Game()
+	: m_Hwnd(nullptr)
+	, m_Width(0)
+	, m_Height(0)
+{
+}
+
+Game::~Game()
+{
+}
+
+void Game::Init(HWND _Hwnd)
+{
+	m_Hwnd = _Hwnd;
+	m_Width = GWinSizeX;
+	m_Height = GWinSizeY;
+}
+
+void Game::Update()
+{
+}
+
+void Game::Render()
+{
+}
+
--- DX2D_Client/Game.h
+++ DX2D_Client/Game.h
@@ -1,5 +1,19 @@
 #pragma once
+
 class Game
 {
+private:
+	HWND		m_Hwnd;
+	int32		m_Width;
+	int32		m_Height;
+
+public:
+	void Init(HWND _Hwnd);
+	void Update();
+	void Render();
+
+public:
+	Game();
+	~Game();
 };
 
