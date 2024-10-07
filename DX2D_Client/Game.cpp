7edf7462a2904d6b1c6723e2785b711f023cba4e
JunmoYang <Juunnmmoo@gmail.com>
2024-10-07 18:20:32 +0900

    Device & SwapChain

--- DX2D_Client/Game.cpp
+++ DX2D_Client/Game.cpp
@@ -5,6 +5,10 @@ Game::Game()
 	: m_Hwnd(nullptr)
 	, m_Width(0)
 	, m_Height(0)
+	, m_ClearColor{0.f, 0.f, 0.f, 0.f}
+	, m_Device(nullptr)
+	, m_DeviceContext(nullptr)
+	, m_SwapChain(nullptr)
 {
 }
 
@@ -12,11 +16,91 @@ Game::~Game()
 {
 }
 
+void Game::CreateDeviceAndSwapChain()
+{
+	DXGI_SWAP_CHAIN_DESC desc;
+	ZeroMemory(&desc, sizeof(desc));
+	{
+		desc.BufferDesc.Width = m_Width;
+		desc.BufferDesc.Height = m_Height;
+		desc.BufferDesc.RefreshRate.Numerator = 60;
+		desc.BufferDesc.RefreshRate.Denominator = 1;
+		desc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		desc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+		desc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+		desc.SampleDesc.Count = 1;
+		desc.SampleDesc.Quality = 0;
+		desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+		desc.BufferCount = 1;
+		desc.OutputWindow = m_Hwnd;
+		desc.Windowed = true;
+		desc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	}
+
+	HRESULT hr = D3D11CreateDeviceAndSwapChain(
+		nullptr,
+		D3D_DRIVER_TYPE_HARDWARE,
+		nullptr,
+		0,
+		nullptr,
+		0,
+		D3D11_SDK_VERSION,
+		&desc,
+		m_SwapChain.GetAddressOf(),
+		m_Device.GetAddressOf(),
+		nullptr,
+		m_DeviceContext.GetAddressOf()
+	);
+	
+	CHECK(hr);
+
+}
+
+void Game::CreateRenderTargetView()
+{
+	HRESULT hr;
+
+	ComPtr<ID3D11Texture2D> backBuffer = nullptr;
+	hr = m_SwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)backBuffer.GetAddressOf());
+	CHECK(hr);
+
+	hr = m_Device->CreateRenderTargetView(backBuffer.Get(), nullptr, m_RenderTargetView.GetAddressOf());
+	CHECK(hr);
+
+}
+
+void Game::SetViewport()
+{
+	m_Viewport.TopLeftX = 0.0f;
+	m_Viewport.TopLeftY = 0.0f;
+	m_Viewport.Width = static_cast<float>(m_Width);
+	m_Viewport.Height = static_cast<float>(m_Height);
+	m_Viewport.MinDepth = 0.0f;
+	m_Viewport.MaxDepth = 1.0f;
+}
+
+void Game::RenderBegine()
+{
+	m_DeviceContext->OMSetRenderTargets(1, m_RenderTargetView.GetAddressOf(), nullptr);
+	m_DeviceContext->ClearRenderTargetView(m_RenderTargetView.Get(), m_ClearColor);
+	m_DeviceContext->RSSetViewports(1, &m_Viewport);
+}
+
+void Game::RenderEnd()
+{
+	// 후면버퍼에 그린 그림을 전면버퍼로 보여달라는 의미.
+	m_SwapChain->Present(1, 0);
+}
+
 void Game::Init(HWND _Hwnd)
 {
 	m_Hwnd = _Hwnd;
 	m_Width = GWinSizeX;
 	m_Height = GWinSizeY;
+
+	CreateDeviceAndSwapChain();
+	CreateRenderTargetView();
+	SetViewport();
 }
 
 void Game::Update()
@@ -25,5 +109,7 @@ void Game::Update()
 
 void Game::Render()
 {
+	RenderBegine();
+	RenderEnd();
 }
 
--- DX2D_Client/Game.h
+++ DX2D_Client/Game.h
@@ -4,8 +4,38 @@ class Game
 {
 private:
 	HWND		m_Hwnd;
-	int32		m_Width;
-	int32		m_Height;
+	uint32		m_Width;
+	uint32		m_Height;
+
+	float			m_ClearColor[4];
+
+private:
+	
+	// Device & SwapChain
+	ComPtr<ID3D11Device>				m_Device;
+	ComPtr<ID3D11DeviceContext>	m_DeviceContext;
+	ComPtr<IDXGISwapChain>			m_SwapChain;
+
+	// RTV
+	ComPtr<ID3D11RenderTargetView>		m_RenderTargetView;
+
+	// ViewPort
+	D3D11_VIEWPORT m_Viewport = { 0 };
+	
+
+
+private:
+	void CreateDeviceAndSwapChain();
+	void CreateRenderTargetView();
+	void SetViewport();
+
+private:
+
+	// 도화지 초기화
+	void RenderBegine();
+	
+	// 도화지 제출
+	void RenderEnd();
 
 public:
 	void Init(HWND _Hwnd);
